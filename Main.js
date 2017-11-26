ebb1f23cfc3f82c11801ca002d412f261274e81c
Yeuda Witman <30429428+YeudaWitman@users.noreply.github.com>
2017-11-26 12:02:38 +0200

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,149 @@
+var dateInput = document.getElementById("dateInput");
+var dateInputDiv = document.getElementById("dateInputDiv"); //use for warning
+var nameInput = document.getElementById("nameInput");
+var detailsInput = document.getElementById("textAreaForm");
+var saveButton = document.getElementById("saveTaskButton");
+var warningDate = document.getElementById("warningDate")
+var dateformat = /^(0?[1-9]|1[012])[\/\-](0?[1-9]|[12][0-9]|3[01])[\/\-]\d{4}$/;
+var notesArea = document.getElementById("notesArea");
+
+//events:
+saveButton.addEventListener("click", newNote);
+window.onload = getFromStorage();
+//window.onload = testStorage()
+
+var notesArray = [];
+
+//function constructor
+function Note(date, name, details) {
+    this.date = date;
+    this.name = name;
+    this.details = details;
+}
+//create new object
+function newNote(e) {
+    e.preventDefault();
+    var n = new Note(dateInput.value, nameInput.value, detailsInput.value);
+    if (validations(n)) {
+        addNoteToView(n);
+        //resetForm();
+        addToArray(n);
+    }
+}
+//form validation--DONE
+function validations(Note) {
+    if (dateformat.test(Note.date)) {
+        console.log("INPUT OK");
+        dateInputDiv.className = 'form-group';
+        saveButton.className = 'btn btn-primary'
+        warningDate.style.visibility = 'hidden';
+        return true
+    } else if (Note.date == "") {
+        console.log("Date Empty");
+        dateInputDiv.className = 'form-group has-error';
+        saveButton.className = 'btn btn-warning'
+        warningDate.style.visibility = 'visible';
+        return false
+    } else {
+        console.log("wrong date format");
+        dateInputDiv.className = 'form-group has-error';
+        saveButton.className = 'btn btn-danger';
+        warningDate.style.visibility = 'visible';
+        return false
+    }
+}
+//reset form values (use preventDefault) --DONE
+function resetForm() {
+    dateInput.value = "";
+    nameInput.value = "";
+    detailsInput.value = "";
+}
+
+function addNoteToView(Note) {
+
+    //create elements
+    var noteDiv = document.createElement('div');
+    const taskNameLabel = document.createElement('div');
+    const buttonsDiv = document.createElement('div');
+    //const editBtn = document.createElement('span');
+    const deleteBtn = document.createElement('span');
+    const textAreaTask = document.createElement('div'); //task detail
+    const taskDateLabel = document.createElement('span'); //taskDateLabel
+
+    //appending
+    noteDiv.appendChild(buttonsDiv);
+    //buttonsDiv.appendChild(editBtn);
+    buttonsDiv.appendChild(deleteBtn);
+    noteDiv.appendChild(taskNameLabel);
+    noteDiv.appendChild(textAreaTask);
+    noteDiv.appendChild(taskDateLabel);
+    notesArea.appendChild(noteDiv);
+
+    //add class names
+    noteDiv.className += "note col-xs-6";
+    buttonsDiv.className += "buttonsContainer";
+    //editBtn.className += "glyphicon glyphicon-edit";
+    deleteBtn.className += "glyphicon glyphicon-remove-sign";
+    taskNameLabel.className += "taskNameLabel";
+    textAreaTask.className += "textAreaTask";
+    //enter new note to beginning:
+    //notesArea.insertBefore(noteDiv, notesArea.childNodes[0]);
+
+    //add attributes - 
+    deleteBtn.setAttribute("title", "Delete Task")
+    //add values
+    taskNameLabel.innerHTML = Note.name;
+    textAreaTask.innerHTML = Note.details;
+    taskDateLabel.innerHTML = Note.date;
+    //fade out
+    noteDiv.style.opacity = "1";
+    //remove button event
+    deleteBtn.addEventListener("click", removeNote);
+
+}
+
+//remove note
+function removeNote(e) {
+    const targetItem = e.target.parentElement.parentElement;
+    targetItem.style.opacity = "0";
+    setTimeout(removeFunc, 1000) //delay removing after the fadeout
+    function removeFunc() {
+        console.log("NOTE REMOVED")
+        targetItem.parentNode.removeChild(targetItem);
+    }
+    console.log("Fade Out");
+    //TODO: remove from storage
+}
+
+//CRUD tasks
+function addToArray(n) {
+    console.log("addToArray " + n);
+    notesArray.push(n);
+    console.log(notesArray);
+    putInStorage(notesArray);
+}
+// add to local storage
+function putInStorage(Note) {
+    localStorage.setItem("notes", JSON.stringify(Note))
+    console.log("put in storage: " + Note);
+}
+
+function getFromStorage() {
+    //check if localstorage empty
+    if (localStorage.getItem("notes") === null) {
+        return false;
+    }
+    var notesStorage = JSON.parse(localStorage.getItem("notes"));
+    Array.from(notesStorage);
+    console.log("getFromStorage " + notesStorage)
+    viewFromStorage(notesStorage);
+}
+// print from storage
+function viewFromStorage(notesStorage) {
+
+
+    for (let j = 0; j < notesStorage.length; j++) {
+        console.log("viewFromStorage " + notesStorage);
+        addNoteToView(notesStorage);
+    }
+};
